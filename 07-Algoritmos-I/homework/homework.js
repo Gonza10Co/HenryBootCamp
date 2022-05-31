74257813c70a4c741a874db614c353bb801dde37
Gonzalo Correa <gonzalo_correa_g@hotmail.com>
2022-05-31 12:47:46 -0500

    tarea algoritmos i

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  const miPrimos = [2, 3, 5, 7, 11];
+  const miArray = [1];
+  let pos = 0;
 
+  while (num > 1) {
+    if (num % miPrimos[pos] === 0) {
+      miArray.push(miPrimos[pos]);
+      num /= miPrimos[pos];
+    } else {
+      pos++
+    }
+  }
+  return miArray
 }
 
 function bubbleSort(array) {
@@ -14,17 +26,36 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j+1]) {
+        let tmp = array[j+1]
+        array[j+1] = array[j];
+        array[j] = tmp;        
+      }
+    }
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i <= array.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (array[j] < array[j-1]) {
+        let tmp = array[j]
+        array[j] = array[j-1];
+        array[j-1] = tmp;
+      }
+    }
+  }
+  return array;
 }
+
+//insertionSort([5, 1, 4, 2, 8])
 
 
 function selectionSort(array) {
@@ -32,9 +63,19 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    let posmin = i;
+    for (let j = i; j < array.length; j++) {
+      if (array[j] < array[posmin]) posmin = j;    
+    }
+    let tmp = array[i]
+    array[i] = array[posmin];
+    array[posmin] = tmp;
+  }
+  return array;
 }
 
+selectionSort([5, 1, 4, 2, 8])
 
 // No modificar nada debajo de esta línea
 // --------------------------------
